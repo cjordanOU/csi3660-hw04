9bf14e88705e83bc2d51d9d076e3121e87119445
cjordanOU <cjordan@oakland.edu>
2021-08-04 19:03:43 -0400

    updated file iteration number 1

--- commitScript.php
+++ commitScript.php
@@ -5,14 +5,13 @@
 	# ----- Iterator File -----
 	$runFile = fopen("runtimes.txt", "r+");
 	$fileSize = filesize("runtimes.txt");
-	if ($fileSize == 0) {
-		$fileSize++;
-	}
+	if ($fileSize == 0) {$fileSize++;}
 	$runTimes = fread($runFile,$fileSize);
 	$runTimes = intval($runTimes); # Converts to integer
 	
 	# ----- Check to see how many times the script has been run -----
 	if ($runTimes < 10) {
+		# If the file has been iterated less than 10 times, iterate and commit
 		$runTimes++;
 		file_put_contents("runtimes.txt", $runTimes);
 		fclose($runFile);
@@ -20,6 +19,7 @@
 		exit("The current Iteration is $runTimes Time(s)\n");
 	}
 	else {
+		# If the file has been iterated 10 times, do nothing
 		fclose($runFile);
 		exit("The file has iterated 10 times\n");
 	}
